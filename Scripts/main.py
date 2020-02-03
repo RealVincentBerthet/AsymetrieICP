7986e09bc2fd664ebcf408b8de47916544a347de
Benoît Quero <benoitquero@MacBook-Pro-de-Benoit.local>
2020-02-03 14:16:51 +0100

    update main

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,6 +1,7 @@
 import csv
+import compute_plane
 import vtk
-
+import numpy as np
 # Read CSV
 # List of data
 data = []
@@ -8,12 +9,17 @@ with open('./Data/ventricules.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
         if (row[0]!="Points:0"):
-            data.append(row)
+            tmp = []
+            for s in row:
+                tmp.append(float(s))
+            data.append(tmp)
 
 
 
 
 def main():
+
+
     colors = vtk.vtkNamedColors()
 
     # Set the background color.
@@ -21,35 +27,37 @@ def main():
 
     # Create a plane
     planeSource = vtk.vtkPlaneSource()
-    planeSource.SetCenter(10, 10, 10)
-    planeSource.SetNormal(10, 2, 3)
-    planeSource.Update()
+    # planeSource.SetCenter(10, 10, 10)
+    # planeSource.SetNormal(10, 2, 3)
 
-    plane = planeSource.GetOutput()
-
-    # Create a mapper and actor
-    mapper = vtk.vtkPolyDataMapper()
-    mapper.SetInputData(plane)
-
-    actor = vtk.vtkActor()
-    actor.SetMapper(mapper)
-    actor.GetProperty().SetColor(colors.GetColor3d("Cyan"))
-
-    # Create a renderer, render window and interactor
-    renderer = vtk.vtkRenderer()
-    renderWindow = vtk.vtkRenderWindow()
-    renderWindow.SetWindowName("Plane")
-    renderWindow.AddRenderer(renderer)
-    renderWindowInteractor = vtk.vtkRenderWindowInteractor()
-    renderWindowInteractor.SetRenderWindow(renderWindow)
-
-    # Add the actors to the scene
-    renderer.AddActor(actor)
-    renderer.SetBackground(colors.GetColor3d("BkgColor"))
-
-    # Render and interact
-    renderWindow.Render()
-    renderWindowInteractor.Start()
+    planeSource = compute_plane.compute_plane_test(planeSource, data)
+    # planeSource.Update()
+    #
+    # plane = planeSource.GetOutput()
+    #
+    # # Create a mapper and actor
+    # mapper = vtk.vtkPolyDataMapper()
+    # mapper.SetInputData(plane)
+    #
+    # actor = vtk.vtkActor()
+    # actor.SetMapper(mapper)
+    # actor.GetProperty().SetColor(colors.GetColor3d("Cyan"))
+    #
+    # # Create a renderer, render window and interactor
+    # renderer = vtk.vtkRenderer()
+    # renderWindow = vtk.vtkRenderWindow()
+    # renderWindow.SetWindowName("Plane")
+    # renderWindow.AddRenderer(renderer)
+    # renderWindowInteractor = vtk.vtkRenderWindowInteractor()
+    # renderWindowInteractor.SetRenderWindow(renderWindow)
+    #
+    # # Add the actors to the scene
+    # renderer.AddActor(actor)
+    # renderer.SetBackground(colors.GetColor3d("BkgColor"))
+    #
+    # # Render and interact
+    # renderWindow.Render()
+    # renderWindowInteractor.Start()
 
 
 if __name__ == '__main__':
